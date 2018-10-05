1b0f85379225d4d2897a8090a675b33171e5568d
benoitbellefontaine <benoit.bellefontaine@gmail.com>
2018-10-05 19:58:31 -0400

    Add files via upload

---
+++ app/containers/ItemPage/fakeItem.js
@@ -0,0 +1,7 @@
+export default {
+    "_id": "5bb7dd35ef7c8607ec525d7e",
+    "picture": "http://placehold.it/1000x400",
+    "name": "Reeves Bishop",
+    "description": "Aute mollit ea enim magna incididunt qui labore pariatur officia qui veniam velit officia. Consectetur irure Lorem ut voluptate amet in excepteur in. Proident duis labore tempor esse sit ipsum. Sint esse ullamco ipsum sit ea non. Consectetur labore commodo amet aliqua eiusmod mollit incididunt.\r\n",
+    "createdAt": "2014-07-30T06:52:39 +04:00"
+}
--- app/containers/ItemPage/index.js
+++ app/containers/ItemPage/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import fakeItem from './fakeItem';
+import ItemDetails from 'components/ItemDetails';
 
+export default class ItemPage extends React.Component {
+    render() {
+        return (
+            <ItemDetails item={fakeItem} />
+        );
+    }
+}
